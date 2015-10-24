f75310b15ec4418b89cf2ff75028f9a0dac76461
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2015-10-24 18:08:43 +0300

    Some code about CL arguments

--- multiboot-cli.py
+++ multiboot-cli.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3 -O
+#!/usr/bin/python3 -O
+import argparse
 import partitioning
 import file_system
 import grub
-
-def usage():
-    pass
 
 def create(opts):
     opts['boot_partition'] = partitioning.make_partition_table(**opts)
@@ -14,15 +12,35 @@ def create(opts):
     grub.configure(opts)
     file_system.unmount(**opts)
 
-dbg_opts = {}
-dbg_opts['pt_type'] = 'gpt'
-dbg_opts['target_device'] = '/dev/sdc'
-dbg_opts['fs_type'] = 'ext2'
-dbg_opts['part_name'] = 'MultiBootFlash'
+def update(opts):
+    pass
 
+def upgrade(opts):
+    pass
+
+def arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    create = subparsers.add_parser('create')
+    create.set_defaults(func=create)
+    
+    update = subparsers.add_parser('update')
+    update.set_defaults(func=update)
+    
+    upgrade = subparsers.add_parser('upgrade')
+    upgrade.set_defaults(func=upgrade)
+
+    return parser.parse_args()
 
 if not __debug__:
-    pass
+    args = arguments()
+    args.func(args)
 else:
+    dbg_opts = {}
+    dbg_opts['pt_type'] = 'gpt'
+    dbg_opts['target_device'] = '/dev/sdc'
+    dbg_opts['fs_type'] = 'ext2'
+    dbg_opts['part_name'] = 'MultiBootFlash'
     create(dbg_opts)
 
